4d75db034b38efef30d91932e0ddaba142c6333d
Mario S. Valdés <mariosergiovaldes145@gmail.com>
2025-06-25 13:31:03 -0500

    typo

--- src/ms_mint_app/plugins/target_optimization.py
+++ src/ms_mint_app/plugins/target_optimization.py
@@ -521,7 +521,7 @@ def callbacks(app, fsc, cache, cpu=None):
         if len(ms_files) == 0:
             return dbc.Alert(
                 'No MS1 files selected for peak optimization in MS-Files tab. '
-                'Please, select some MS1files in column "use_for_optimization". This not apply to MS2 files.',
+                'Please, select some MS1files in column "use_for_optimization". This does not apply to MS2 files.',
                 color="warning",
             )
         else:
