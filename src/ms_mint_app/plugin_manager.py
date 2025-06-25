cb01eec73114fc156a9b17a8e97e1b72a91b4f5b
Mario S. Valdés <mariosergiovaldes145@gmail.com>
2025-06-25 10:26:49 -0500

    disabled metadata plugin

--- src/ms_mint_app/plugin_manager.py
+++ src/ms_mint_app/plugin_manager.py
@@ -26,12 +26,12 @@ class PluginManager:
         # Register built-in plugins with order attribute
         self.register_plugin("Workspaces", WorkspacesPlugin())
         self.register_plugin("MS-Files", MsFilesPlugin())
-        self.register_plugin("Metadata", MetadataPlugin())
+        # self.register_plugin("Metadata", MetadataPlugin())
         self.register_plugin("Targets", TargetsPlugin())
         #self.register_plugin("Add Metabolites", AddMetabolitesPlugin())
         self.register_plugin("Optimization", TargetOptimizationPlugin())
         # add the new MS2 browser plugin
-        self.register_plugin("MS2 Browser", MS2BrowserPlugin())
+        # self.register_plugin("MS2 Browser", MS2BrowserPlugin())
         self.register_plugin("Processing", ProcessingPlugin())
         self.register_plugin("Quality Control", QualityControlPlugin())
         self.register_plugin("Analysis", AnalysisPlugin())
