15ce30bb88e0e91426a08b720bc65d54e4bf0dc5
Mario S. Valdés <mariosergiovaldes145@gmail.com>
2025-10-29 16:55:56 -0500

    comment out unimplemented plugin registrations in plugin_manager.py

--- src/ms_mint_app/plugin_manager.py
+++ src/ms_mint_app/plugin_manager.py
@@ -26,10 +26,10 @@ class PluginManager:
         #self.register_plugin("Add Metabolites", AddMetabolitesPlugin())
         self.register_plugin("Optimization", TargetOptimizationPlugin())
         # add the new MS2 browser plugin
-        self.register_plugin("MS2 Browser", MS2BrowserPlugin())
+        # self.register_plugin("MS2 Browser", MS2BrowserPlugin())
         self.register_plugin("Processing", ProcessingPlugin())
-        self.register_plugin("Quality Control", QualityControlPlugin())
-        self.register_plugin("Analysis", AnalysisPlugin())
+        # self.register_plugin("Quality Control", QualityControlPlugin())
+        # self.register_plugin("Analysis", AnalysisPlugin())
 
         # Discover and register external plugins
         if sys.version_info >= (3, 10):
