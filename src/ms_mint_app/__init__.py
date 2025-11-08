5e06a7b4ad8b3f59b0784b09c41abebd59e482e8
Mario S. Valdés <mariosergiovaldes145@gmail.com>
2025-11-08 18:40:36 -0500

    refactor version management to use versioneer and update dependencies

--- src/ms_mint_app/__init__.py
+++ src/ms_mint_app/__init__.py
@@ -1,2 +1,5 @@
-from ._version import version as __version__
-from ._version import version_tuple
+from ._version import get_versions
+__version__ = get_versions().get("version", "0+unknown")
+del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
