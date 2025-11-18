353c377e5a1a44cbcbfe310b82cb016823d5f132
Mario S. Valdés <mariosergiovaldes145@gmail.com>
2025-11-17 19:12:26 -0500

    refactor version management to simplify version retrieval

--- src/ms_mint_app/__init__.py
+++ src/ms_mint_app/__init__.py
@@ -1,5 +1,3 @@
 from ._version import get_versions
 __version__ = get_versions().get("version", "0+unknown")
 del get_versions
-from . import _version
-__version__ = _version.get_versions()['version']
