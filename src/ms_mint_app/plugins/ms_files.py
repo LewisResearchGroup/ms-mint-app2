54862f26793eec1e385c477e74e97b35f0d2edc7
Mario S. Valdés <mariosergiovaldes145@gmail.com>
2025-11-23 19:10:45 -0500

    added total number of records as page size option to show items

--- src/ms_mint_app/plugins/ms_files.py
+++ src/ms_mint_app/plugins/ms_files.py
@@ -561,7 +561,8 @@ def callbacks(cls, app, fsc, cache, args_namespace):
             return [
                 data.to_dicts(),
                 [],
-                {**pagination, 'total': number_records, 'current': current},
+                {**pagination, 'total': number_records, 'current': current, 'pageSizeOptions': [5, 10, 25, 50, 100,
+                                                                                                number_records]},
                 output_filterOptions
             ]
         return dash.no_update
